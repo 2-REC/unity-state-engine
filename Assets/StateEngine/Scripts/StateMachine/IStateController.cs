423f97f91cbdf9fb71ca09de0620c1d56c2ec4e3
Derek Severin <severinderek@gmail.com>
2025-10-16 17:13:05 +0700

    state controller script order comment

--- Assets/StateEngine/Scripts/StateMachine/IStateController.cs
+++ Assets/StateEngine/Scripts/StateMachine/IStateController.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// TODO: want this? (might not be a good idea)
+//[DefaultExecutionOrder(-5)]
 public abstract class IStateController : MonoBehaviour {
 
     public int StateId { get; private set; }
